c0298c788346b2abf7df599caa0b5da7be9b8f10
Muhammad Hoosen <Hoosenmuhammad@gmail.com>
2026-04-21 20:39:32 +0200

    Commit 5 - Added login and duplicate queries to UserDao

--- app/src/main/java/com/example/pocketpenny/data/UserDao.kt
+++ app/src/main/java/com/example/pocketpenny/data/UserDao.kt
@@ -6,4 +6,13 @@ import androidx.room.*
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.ABORT)
     suspend fun registerUser(user: User)
+
+    @Query("SELECT * FROM user_table WHERE email = :email LIMIT 1")
+    suspend fun getUserByEmail(email: String): User?
+
+    @Query("SELECT EXISTS(SELECT 1 FROM user_table WHERE username = :username)")
+    suspend fun checkIfUsernameExists(username: String): Boolean
+
+    @Query("SELECT EXISTS(SELECT 1 FROM user_table WHERE email = :email)")
+    suspend fun checkIfEmailExists(email: String): Boolean
 }
