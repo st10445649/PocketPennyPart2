60e69ebfca57f1865fb882256a286968803a4083
ST10249469 <ST10249469@vcconnect.edu.za>
2026-04-27 21:28:38 +0200

    Added budget table to Room database

--- app/src/main/java/com/example/pocketpenny/data/AppDatabase.kt
+++ app/src/main/java/com/example/pocketpenny/data/AppDatabase.kt
@@ -5,7 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class, Expense::class, Category::class], version = 2, exportSchema = false)
+@Database(
+    entities = [User::class, Expense::class, Category::class, Budget::class],
+    version = 3,
+    exportSchema = false
+)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao(): UserDao
