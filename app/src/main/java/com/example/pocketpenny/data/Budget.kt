052d1249de43bf718797ca5a51ea5fa9c9af07c5
ST10249469 <ST10249469@vcconnect.edu.za>
2026-04-27 21:19:08 +0200

    Added budget data model

---
+++ app/src/main/java/com/example/pocketpenny/data/Budget.kt
@@ -0,0 +1,14 @@
+package com.example.pocketpenny.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "budget_table")
+data class Budget(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val categoryId: Int,
+    val categoryName: String,
+    val minAmount: Double,
+    val maxAmount: Double
+)
